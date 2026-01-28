d7779cbc0396d7a7045405b77d0802b3bef15f25
lmupskillpt <lumipate>
2026-01-28 02:42:11 +0000

    fixed header sizing, did some visual tweaks

--- utils.js
+++
@@ -1,3 +0,0 @@
-const themeToggle = document.getElementById('themeToggle');
-themeToggle.addEventListener('change', () => document.documentElement.dataset.theme = themeToggle.checked ? 'dark' : 'light');
-themeToggle.checked = localStorage.theme === 'dark';
